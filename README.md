cb161a8a042b60aa9bb7b283572d4fdc14fc3094
Jordan Davidson <jordandavidson744@yahoo.com>
2020-06-29 11:46:15 -0400

    finished frontend-shoestore

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+backend api: https://github.com/Jordan-Davidson/shoeStore
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {Shoes: []}
+  }
+  componentDidMount(){
+    fetch('http://127.0.0.1:8000/api/shoe/')
+    .then(res => res.json())
+    .then(res => this.setState({Shoes: res}))
+  }
+  render() {
+    return (
+      <div>
+        <h1>Shoestore</h1>
+        <ul>{this.state.Shoes.map(shoe =>{
+              return(
+                <>
+                <li>
+                  <ul key = {shoe.size}>
+                    Size : {shoe.size}
+                  </ul>
+                  <ul key = {shoe.manufacturer}>
+                    Manufacturer : {shoe.manufacturer}
+                  </ul>
+                  <ul key = {shoe.color}>
+                    Color : {shoe.color}
+                  </ul>
+                  <ul key = {shoe.material}>
+                    Material : {shoe.material}
+                  </ul>
+                  <ul key = {shoe.shoe_type}>
+                    Shoe Type : {shoe.shoe_type}
+                  </ul>
+                  <ul key = {shoe.fasten_type}>
+                    Fasten Type : {shoe.fasten_type}
+                  </ul>
+                </li>
+                <br></br>
+                </>
+            )
+            })}</ul>
+      </div>
+    )
+  }
 }
 
 export default App;
